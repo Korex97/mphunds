cd859fd16f98b4536a9195e8f15db2f675153a1d
korede <egbeolowoakorede97@gmail.com>
2021-05-14 13:42:21 +0100

    Mphunds

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,39 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
+
+router.get('/plan', function(req, res, next) {
+  res.render('plan');
+});
+
+router.get('/coupon', function(req, res, next) {
+  res.render('coupon');
+});
+
+router.get('/gen-income', function(req, res, next) {
+  res.render('gen-income');
+});
+
+router.get('/withdraw', function(req, res, next) {
+  res.render('withdraw');
+});
+
+router.get('/tos', function(req, res, next) {
+  res.render('tos');
+});
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
 module.exports = router;
