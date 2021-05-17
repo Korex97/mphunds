94b542212a1aa59941e005b2c260b7856119ba3f
korede <egbeolowoakorede97@gmail.com>
2021-05-17 12:34:54 +0100

    Mphund

--- config/passport.js
+++ config/passport.js
@@ -18,25 +18,28 @@ module.exports = function(passport) {
 
     passport.use("local-login", new LocalStrategy({
         usernameField: 'email',
-        passwordField: 'password',
         passReqToCallback: true
-    }, (req, email, password, done) => {
+    }, (email, password, done) => {
         User.findOne({ 'email': email}, (err, user) => {
             if (err) return done(err);
             console.log("user", user);
             console.log('password', password);
 
             if (!user){
-                return done(null, false, req.flash("message", "Email not registered"));
+                return done(null, false, {"message": "Email not registered"});
             }
-            if (user.validPassword(password))
-                return done(
-                    null,
-                    false,
-                    req.flash("message", "Wrong Password")
-                )
-            return done(null, user)
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) throw err;
+
+                if (isMatch) {
+                    return done(null, user)
+                } else {
+                    return done(null, false, {message: "Password Incorrect"})
+                }
+
+            })
         })
+        .catch(err => console.log(err));
     
     }
     ))
