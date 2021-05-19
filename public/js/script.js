9329ecd24c83c45501f4936e6d1d72ef8f56ea7b
korede <egbeolowoakorede97@gmail.com>
2021-05-19 10:44:30 +0100

    mphunds

--- public/js/script.js
+++ public/js/script.js
@@ -1,45 +1,6 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarlinks = document.getElementsByClassName('navbar-links')[0]
+const toggleBtn = document.getElementsByClassName('btn')[0]
+const navUl = document.getElementsByClassName('navul')[0]
 
-toggleButton.addEventListener('click', () => {
-	navbarlinks.classList.toggle('active')
+toggleBtn.addEventListener('click', () => {
+	navUl.classList.toggle('active')
 })
-
-
-//modal one
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-	button.addEventListener('click', () => {
-		const modal = document.querySelector(button.dataset.modalTarget)
-		openModal(modal)
-	})
-})
-
-overlay.addEventListener('click', () => {
-	const modals = document.querySelectorAll('.modal.active')
-	modals.forEach(modal => {
-		closeModal(modal)
-	})
-})
-
-closeModalButtons.forEach(button => {
-	button.addEventListener('click', () => {
-		const modal = button.closest('.modal')
-		closeModal(modal)
-	})
-})
-
-function openModal(modal) {
-	if (modal == null) return
-	modal.classList.add('active')
-	overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-	if (modal == null) return
-	modal.classList.remove('active')
-	overlay.classList.remove('active')
-}
