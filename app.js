bbcc665641d49f0b14f19c2f7edb4523b579e0de
korede <egbeolowoakorede97@gmail.com>
2021-05-20 16:43:10 +0100

    mphunds

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/', indexRouter.userRoutes);
-app.use('/vendors', indexRouter.vendorRoutes);
+app.use('/vendors/', indexRouter.vendorRoutes);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
